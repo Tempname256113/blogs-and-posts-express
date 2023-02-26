5e095aca32a77d7d088aeb6e361fd96fc0241724
Alexandr <smolnikov.456@mail.ru>
2023-02-26 13:39:48 +0500

    try to catch error with rate limiter

--- src/middlewares/request-limiter-middleware.ts
+++ src/middlewares/request-limiter-middleware.ts
@@ -18,12 +18,14 @@ export const requestLimiterMiddleware = async (req: Request, res: Response, next
     const interval = 10 * 1000
     const currentIp: string = req.ip;
     const routeUrl: string = req.url;
+    console.log(routeUrl, 'current url')
+    console.log(currentIp, 'currentIp')
     const currentDate = new Date()
     const attemptsTime = new Date(currentDate.getTime() - interval)
     const attemptsCount: number = await requestLimiterRepository.findBannedIpAddress({ip: currentIp, routeUrl, time: attemptsTime});
     await requestLimiterRepository.addRequestData({ip: currentIp, routeUrl, time: currentDate})
     if(attemptsCount < 5) {
-        console.log(attemptsCount)
+        console.log(attemptsCount, 'attempts count')
         next();
     } else {
         res.sendStatus(429)
